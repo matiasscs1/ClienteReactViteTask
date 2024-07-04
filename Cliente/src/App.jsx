7a1ab4b1bd7862f41011e03e2554ac1bc48a224c
matiasscs1 <matias.cedeno@udla.edu.ec>
2024-07-04 16:55:36 -0500

    2  commit

--- Cliente/src/App.jsx
+++ Cliente/src/App.jsx
@@ -1,14 +1,11 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { ToastContainer } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
 import Tareas from './view/Tareas.jsx';
 import User from './view/User.jsx';
 
 function App() {
   return (
     <>
-      <ToastContainer />
       <BrowserRouter>
         <Routes>
           <Route path="/tareas-page" element={<Tareas />} />
